57975a489f404a6da9b5af5a25d8487a5b58e0ad
Scott Schneider <sschneider@puppetlabs.com>
2013-11-08 10:48:51 -0800

    Log to a (configurable) file

--- lib/logger.rb
+++ lib/logger.rb
@@ -1,13 +1,20 @@
 require 'rubygems' unless defined?(Gem)
 
 class Logger
-  def initialize
+  def initialize(
+    f = '/var/log/vmware-host-pooler.log'
+  )
+    @file = f
   end
 
   def log level, string
     time = Time.new
     stamp = time.strftime('%Y-%m-%d %H:%M:%S')
     puts "[#{stamp}] #{string}"
+
+    open(@file, 'a') do |f|
+      f.puts "[#{stamp}] #{string}"
+    end
   end
 end
 
