fb323a70bfc248734077bcaa88824bca512b3e54
Rick Bradley <rick@rickbradley.com>
2016-07-12 16:50:50 -0500

    [QENG-4075] Factor out Vmpooler::DummyStatsd
    This makes it visible to lib/vmpooler.rb, as well as putting this dummy
    metrics endpoint in its own file for easier discovery.

--- lib/vmpooler/statsd.rb
+++ lib/vmpooler/statsd.rb
@@ -34,23 +34,4 @@ module Vmpooler
       $stderr.puts "Failure updating timing #{prefix}.#{label} on statsd server [#{server}:#{port}]: #{err}"
     end
   end
-
-  class DummyStatsd
-    attr_reader :server, :port, :prefix
-
-    def initialize(params = {})
-    end
-
-    def increment(label)
-      true
-    end
-
-    def gauge(label, value)
-      true
-    end
-
-    def timing(label, duration)
-      true
-    end
-  end
 end
