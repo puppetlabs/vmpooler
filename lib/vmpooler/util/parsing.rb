f59b87a7ff97718400adbfcad4c0655849290955
Samuel Beaulieu <samuel@puppetlabs.com>
2020-06-11 11:05:43 -0500

    add some metrics to the od request generation

--- lib/vmpooler/util/parsing.rb
+++ lib/vmpooler/util/parsing.rb
@@ -1,11 +1,11 @@
 # utility class shared between apps
 module Vmpooler
   class Parsing
-    def get_platform_pool_count(requested, &block)
+    def self.get_platform_pool_count(requested, &block)
       requested_platforms = requested.split(',')
       requested_platforms.each do |platform|
         platform_alias, pool, count = platform.split(':')
-        #raise ArgumentError if platform_alias.nil? || pool.nil? || count.nil?
+        raise ArgumentError if platform_alias.nil? || pool.nil? || count.nil?
         yield platform_alias, pool, count
       end
     end
